0f08177270e1a13fd5fe6f564f6bf1d6a98f6ad0
Nadah Khaled <nadahkhaledd@gmail.com>
2022-11-30 15:23:33 +0200

    - add updateOrderService()

--- src/main/java/org/example/service/admin/AdminServiceImplementation.java
+++ src/main/java/org/example/service/admin/AdminServiceImplementation.java
@@ -48,6 +48,9 @@ public class AdminServiceImplementation implements AdminService{
         return rowsAffected==1;
     }
 
+    /**
+     * @inheritDoc
+     */
     @Override
     public void updateOrderStatus(int orderID) {
         /// call orderservice.updateStatus()
