7453379903a723957ed366b61b87ad7848029573
Nadah Khaled <nadahkhaledd@gmail.com>
2022-11-30 12:47:19 +0200

    - add admin repo prototypes

--- src/main/java/org/example/repository/admin/AdminRepository.java
+++ src/main/java/org/example/repository/admin/AdminRepository.java
@@ -1,5 +1,15 @@
 package org.example.repository.admin;
 
+import org.example.entity.Admin;
+import org.example.entity.Category;
+
 public interface AdminRepository {
+    void addAdmin(Admin admin);
+    void updateAdmin(Admin admin);
+    void removeAdmin(int adminID);
+    void suspendCustomer(int customerID);
+    void addCategory(Category category);
+    void updateCategory(int categoryID, String imgPath);
+    void removeCategory(int categoryID);
 
 }
--- src/main/java/org/example/repository/admin/AdminRepositoryImplementation.java
+++ src/main/java/org/example/repository/admin/AdminRepositoryImplementation.java
@@ -1,5 +1,42 @@
 package org.example.repository.admin;
 
+import org.example.entity.Admin;
+import org.example.entity.Category;
+
 public class AdminRepositoryImplementation implements AdminRepository{
 
+    @Override
+    public void addAdmin(Admin admin) {
+
+    }
+
+    @Override
+    public void updateAdmin(Admin admin) {
+
+    }
+
+    @Override
+    public void removeAdmin(int adminID) {
+
+    }
+
+    @Override
+    public void suspendCustomer(int customerID) {
+
+    }
+
+    @Override
+    public void addCategory(Category category) {
+
+    }
+
+    @Override
+    public void updateCategory(int categoryID, String imgPath) {
+
+    }
+
+    @Override
+    public void removeCategory(int categoryID) {
+
+    }
 }
