84aa67823025518ffb44e821c622ea374e09f406
Nadah Khaled <nadahkhaledd@gmail.com>
2022-11-30 16:53:59 +0200

    - add test main

--- src/main/java/org/example/main/Main.java
+++ src/main/java/org/example/main/Main.java
@@ -1,10 +1,28 @@
 package org.example.main;
 
 import org.example.configuration.AppConfig;
+import org.example.entity.Admin;
+import org.example.enums.Gender;
+import org.example.service.admin.AdminService;
+import org.example.service.admin.AdminServiceImplementation;
+import org.example.utility.DateUtils;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context=new AnnotationConfigApplicationContext(AppConfig.class);
+//        DateUtils dateUtils = new DateUtils();
+//        AnnotationConfigApplicationContext context=new AnnotationConfigApplicationContext(AppConfig.class);
+//        AdminService adminService = context.getBean(AdminServiceImplementation.class);
+//
+//        Date date = dateUtils.convertStringToDate("1999-4-23");
+//
+//        Admin admin = new Admin("hagar", "admin",
+//                "adminhagar@shop.com", "adm1nh@g@r",
+//                Gender.female, date);
+//
+//        adminService.addAdmin(admin);
+
     }
 }
