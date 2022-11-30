dc6085f75dfcdcb8443d8b3a9f05fdf8174c7c6c
Omar Abu Elkheir <omar.ramzi97@gmail.com>
2022-11-30 11:18:15 +0200

    feat : intialize order endpoints

--- src/main/java/org/example/controller/CustomerController.java
+++ src/main/java/org/example/controller/CustomerController.java
@@ -1,6 +1,9 @@
 package org.example.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
--- src/main/java/org/example/controller/OrderController.java
+++ src/main/java/org/example/controller/OrderController.java
@@ -1,4 +1,38 @@
 package org.example.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/orders")
 public class OrderController {
+
+    @GetMapping(name = "/getOrders")
+    public String getOrders(Long userId){
+        return "";
+    }
+
+    @GetMapping(name="/getOrderDetails")
+    public String getOrderDetails(Long userId, Long orderId){
+        return "";
+    }
+
+    @GetMapping(name = "/cancelOrder")
+    public String cancelOrder(Long orderId,Long userId){
+        return "";
+    }
+
+    @DeleteMapping(name = "/deleteOrderItem")
+    //before order approval
+    public String deleteOrderItem(Long productId, Long userId){
+        return "";
+    }
+
+    @PutMapping(name = "/updateOrderItemQty")
+    public String updateOrderDetailQty(Long productId, Long userId, int Qty){
+        return "";
+    }
+
 }
