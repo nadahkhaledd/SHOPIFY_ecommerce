d6fc94d5889d85b1c23c50cf644ecd4118d96b01
Nadah Khaled <nadahkhaledd@gmail.com>
2022-12-02 17:12:19 +0200

    - add category controller

--- src/main/java/org/example/controller/AdminController.java
+++ src/main/java/org/example/controller/AdminController.java
@@ -1,8 +1,11 @@
 package org.example.controller;
 
 import org.example.entity.Admin;
+import org.example.entity.Category;
+import org.example.entity.Product;
 import org.example.enums.Gender;
 import org.example.service.admin.AdminService;
+import org.example.service.category.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,32 +26,54 @@ import java.util.Map;
 public class AdminController {
 
     private final AdminService adminService;
+    private final CategoryService categoryService;
 
     @Autowired
-    public AdminController(AdminService adminService) {
+    public AdminController(AdminService adminService, CategoryService categoryService) {
         this.adminService = adminService;
+        this.categoryService = categoryService;
     }
 
-    @GetMapping("addadmin")
+    @GetMapping("addAdmin")
     public String newAdmin(Model model) {
 
         List<String> genders = new ArrayList<>(
                 Arrays.asList(Gender.male.toString(), Gender.female.toString()));
         model.addAttribute("admin", new Admin());
         //model.addAllAttributes(Map.of("admin", new Admin(), "gender", genders));
-        return "addadmin";
+        return "addAdmin";
     }
 
-    @PostMapping("addadmin")
+    @PostMapping("addAdmin")
     public String addUser(@Valid  @ModelAttribute("admin") Admin admin, BindingResult bindingResult) {
         System.out.println("hello to post admin");
         if (bindingResult.hasErrors()) {
             Map<String, Object> model = bindingResult.getModel();
             System.out.println(model);
-            return "addadmin";
+            return "addAdmin";
         }
         System.out.println(admin);
         adminService.addAdmin(admin);
+        return "redirect:/admin/login";
+    }
+
+    @GetMapping("addCategory")
+    public String newCategory(Model model) {
+
+        model.addAttribute("category", new Category());
+        return "addCategory";
+    }
+
+    @PostMapping("addCategory")
+    public String addCategory(@Valid  @ModelAttribute("category") Category category, BindingResult bindingResult) {
+        System.out.println("hello to post category");
+        if (bindingResult.hasErrors()) {
+            Map<String, Object> model = bindingResult.getModel();
+            System.out.println(model);
+            return "addCategory";
+        }
+        System.out.println(category);
+        categoryService.addCategory(category);
         return "redirect:/admin/login";
     }
 
--- src/main/webapp/WEB-INF/views/addAdmin.jsp
+++ src/main/webapp/WEB-INF/views/addAdmin.jsp
@@ -7,7 +7,7 @@
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     <!-- Customized Bootstrap Stylesheet -->
-    <link href="${pageContext.request.contextPath}/resources/css/newadmin.css" rel="stylesheet">
+    <link href="${pageContext.request.contextPath}/resources/css/newMember.css" rel="stylesheet">
   </head>
   <body>
     <div class="container" id="container">
