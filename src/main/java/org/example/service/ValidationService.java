1bd82b48d6c203df1437c2b01482b3031d1ecddc
hagar110 <hagar.ehab999@gmail.com>
2022-12-07 12:49:29 +0200

    admin email validation

--- src/main/java/org/example/service/ValidationService.java
+++ src/main/java/org/example/service/ValidationService.java
@@ -5,41 +5,21 @@ import org.example.service.security.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @Service
 public class ValidationService {
   public Response validateAdminEmail(String email){//check admin email domain is shopify
       String domain;
-      try {
-          domain = email.substring(email.indexOf("@") + 1);
-          domain = domain.substring(0, domain.indexOf("."));
-          System.out.println("domain:: "+domain);
-      }
-      catch (Exception e){
-          System.out.println(e.getStackTrace());
-          return new Response("error occurred while processing your request",500,true);
-      }
-      boolean isInvalidEmailFormat=validateEmailFormat(email).isErrorOccurred();
-
-      if(!domain.equalsIgnoreCase("shopify") || isInvalidEmailFormat ){
-          System.out.println("in validation serviceee "+isInvalidEmailFormat);
+      String regex = "[a-z0-9]+@shopify.com";
+      Pattern pattern = Pattern.compile(regex);
+      Matcher matcher = pattern.matcher(email);
+      if(!matcher.matches()){
+          System.out.println("in validation serviceee ");
           return new Response("Wrong domain",400,true,true);
       }
       return new Response("Done",200,false,false);
   }
-  public Response validateEmailFormat(String email){
-      // check if email doesn't exist in the database
-      String domain;//checking if email ends with .com
-      try {
-          domain = email.substring(email.length()-4);
-          System.out.println("domain:: "+domain);
-      }
-      catch (Exception e){
-          System.out.println(e.getStackTrace());
-          return new Response("error occurred while processing your request",500,true);
-      }
-      if (!domain.equals(".com")){
-          return new Response("Wrong domain",400,true);
-      }
-      return new Response("Done",200,false);
-  }
+
 }
