5eafe11104a252e74218137a7c0c21cabaaa868a
Omar Abu Elkheir <omar.ramzi97@gmail.com>
2022-12-07 13:10:14 +0200

    feat : validate user email

--- src/main/java/org/example/controller/AuthController.java
+++ src/main/java/org/example/controller/AuthController.java
@@ -42,8 +42,8 @@ public class AuthController {
 
     @PostMapping("/register")
     public String register(@Valid @DateTimeFormat(pattern = "yyyy-MM-dd") @ModelAttribute("user") Customer user,Model model){
-        String regex = "[a-z0-9]+@shopify.com";
-        Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);        Matcher matcher = pattern.matcher(user.getEmail());
+        Pattern pattern = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(user.getEmail());
         if(!matcher.matches()){
             model.addAttribute("error","Please enter a valid Email");
             return "redirect:/register";
