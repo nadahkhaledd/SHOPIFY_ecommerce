fa5671459705c5184496ce83db4cc70a4a368d79
Omar Abu Elkheir <omar.ramzi97@gmail.com>
2022-12-14 17:07:13 +0200

    refactor: Unit testing AdminEmailChecker final

--- src/test/java/ValidationTest.java
+++ src/test/java/ValidationTest.java
@@ -1,17 +1,8 @@
 import org.example.model.Response;
-import org.example.repository.order.OrderRepositoryImpl;
-import org.example.repository.product.ProductRepoImpl;
-import org.example.repository.user.UserRepositoryImpl;
 import org.example.service.ValidationService;
-import org.example.service.order.OrderServiceImpl;
-import org.example.service.product.ProductServiceImpl;
-import org.example.service.user.UserServiceImpl;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
-
 import static org.junit.Assert.assertEquals;
-
 public class ValidationTest {
     private ValidationService validationService;
 
