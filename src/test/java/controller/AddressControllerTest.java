86f4a13fc56caff6ffb9e8cb27c5f81b38c0ec44
YoussefMedhat <ym.desouky@gmail.com>
2022-12-15 15:09:58 +0200

    Service test fixed.

--- src/test/java/controller/AddressControllerTest.java
+++ src/test/java/controller/AddressControllerTest.java
@@ -17,10 +17,11 @@ import org.springframework.validation.BindingResult;
 
 import javax.servlet.http.HttpSession;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
 
 public class AddressControllerTest {
     private final AddressController addressController;
@@ -82,4 +83,134 @@ public class AddressControllerTest {
 //        //Assert
 //        assertEquals("error", returnedPage);
 //    }
+
+    @Test
+    public void deleteAddressTest_sendAddressId_returnViewAddressesPage() {
+        //Arrange
+        Response response = new Response("Done", 200, false, false, true);
+        when(addressServiceMock.deleteAddress(anyInt())).thenReturn(response);
+
+        //Act
+        String returnedPage = addressController.deleteAddress(1, modelMock);
+
+        //Assert
+        assertEquals("redirect:/address/view", returnedPage);
+    }
+
+    @Test
+    public void deleteAddressTest_sendAddressId_ExceptedOccurred_returnErrorPage() {
+        //Arrange
+        Response response = new Response("Done", 200, true, false, false);
+        when(addressServiceMock.deleteAddress(anyInt())).thenReturn(response);
+
+        //Act
+        String returnedPage = addressController.deleteAddress(1, modelMock);
+
+        //Assert
+        assertEquals("error", returnedPage);
+    }
+
+    @Test
+    public void newAddressTest_sendAddressId_returnUpdateAddressPage() {
+        //Arrange
+        Response response = new Response("Done", 200, false, false, new Address());
+        when(addressServiceMock.getAddressById(anyInt())).thenReturn(response);
+
+        //Act
+        String returnedPage = addressController.newAddress(1, modelMock);
+
+        //Assert
+        assertEquals("updateAddress", returnedPage);
+        verify(modelMock, times(1)).addAttribute(anyString(), any());
+    }
+
+    @Test
+    public void updateAddressTest_sendAddress_validationErrorOccurred_returnUpdateAddressPage() {
+        //Arrange
+        when(bindingResultMock.hasErrors()).thenReturn(true);
+        Address address = new Address();
+
+        //Act
+        String returnedPage = addressController.updateAddress(address, bindingResultMock, modelMock, httpSessionMock);
+
+        //Assert
+        assertEquals("updateAddress", returnedPage);
+    }
+
+//    @Test
+//    public void updateAddressTest_sendAddress_fieldErrorOccurred_returnUpdateAddressPage() {
+//        //Arrange
+//        when(httpSessionMock.getAttribute(anyString())).thenReturn(1);
+//        when(userServiceMock.getUserById(anyInt())).thenReturn(new Response<>("Done", 200, false, false, new User()));
+//        Response response = new Response("error", 400, true, true, false);
+//        when(addressServiceMock.updateAddress(any(Address.class))).thenReturn(response);
+//        Address address = new Address();
+//
+//        //Act
+//        String returnedPage = addressController.updateAddress(address, bindingResultMock, modelMock, httpSessionMock);
+//
+//        //Assert
+//        assertEquals("updateAddress", returnedPage);
+//    }
+//
+//    @Test
+//    public void updateAddressTest_sendAddress_internalErrorOccurred_returnErrorPage() {
+//        //Arrange
+//        when(httpSessionMock.getAttribute(anyString())).thenReturn(1);
+//        when(userServiceMock.getUserById(anyInt())).thenReturn(new Response<>("Done", 200, false, false, new User()));
+//        Response response = new Response("error", 400, true, false, false);
+//        when(addressServiceMock.updateAddress(any(Address.class))).thenReturn(response);
+//        Address address = new Address();
+//
+//        //Act
+//        String returnedPage = addressController.updateAddress(address, bindingResultMock, modelMock, httpSessionMock);
+//
+//        //Assert
+//        assertEquals("error", returnedPage);
+//    }
+//
+//    @Test
+//    public void updateAddressTest_sendAddress_returnViewAddressesPage() {
+//        //Arrange
+//        when(httpSessionMock.getAttribute(anyString())).thenReturn(1);
+//        when(userServiceMock.getUserById(anyInt())).thenReturn(new Response<>("Done", 200, false, false, new User()));
+//        Response response = new Response("Done", 200, false, false, true);
+//        when(addressServiceMock.updateAddress(any(Address.class))).thenReturn(response);
+//        Address address = new Address();
+//
+//        //Act
+//        String returnedPage = addressController.updateAddress(address, bindingResultMock, modelMock, httpSessionMock);
+//
+//        //Assert
+//        assertEquals("redirect:/address/view", returnedPage);
+//    }
+
+    @Test
+    public void getAddressesTest_internalErrorOccurred_returnErrorPage() {
+        //Arrange
+        when(httpSessionMock.getAttribute(anyString())).thenReturn(1);
+        Response response = new Response("Done", 200, true, false, new ArrayList<>());
+        when(addressServiceMock.getUserAddresses(anyInt())).thenReturn(response);
+
+        //Act
+        String returnedPage = addressController.getAddresses(modelMock, httpSessionMock);
+
+        //Assert
+        assertEquals("error", returnedPage);
+    }
+
+    @Test
+    public void getAddressesTest_returnViewAddressesPage() {
+        //Arrange
+        when(httpSessionMock.getAttribute(anyString())).thenReturn(1);
+        Response response = new Response("Done", 200, false, false, new ArrayList<>());
+        when(addressServiceMock.getUserAddresses(anyInt())).thenReturn(response);
+
+        //Act
+        String returnedPage = addressController.getAddresses(modelMock, httpSessionMock);
+
+        //Assert
+        assertEquals("viewAddresses", returnedPage);
+    }
+
 }
